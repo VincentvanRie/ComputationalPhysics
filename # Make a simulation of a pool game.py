32ce9546028c80e4e212450183e8527b9f24774a
VincentvanRie <vincent.vanrie01@gmail.com>
2024-02-23 11:52:11 +0100

    initial commit

---
+++ # Make a simulation of a pool game.py
@@ -0,0 +1,11 @@
+def class Argon:
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.radius = 0.5
+        self.mass = 1
+    
+    # Particle Argon
+    def Pote
